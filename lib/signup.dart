ba99f14e5efb5b4072fd81b0f93e16b9c67385d9
Kollin <Bartzkol@msu.edu>
2022-04-09 07:33:04 -0400

    sign up

--- lib/signup.dart
+++ lib/signup.dart
@@ -8,12 +8,37 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  /*
+   * The variables that store the information in the 3 boxes
+   */
   TextEditingController username = TextEditingController();
   TextEditingController password1 = TextEditingController();
   TextEditingController password2 = TextEditingController();
+
+  /*
+   * Sends the error message for if the attempted sign up information is invalid
+   * :param: priority: determines which message would be shown for which box
+   * :return: the error message, if no errors arise for that box, returns null
+   */
+  String? errorMessages(int priority) {
+    if (username.text.length < 5 && priority == 0) {
+      return "Username is too short";
+    }
+    if (password1.text.length < 7 && priority == 1) {
+      return "Password is too short";
+    }
+    if (password1.text != password2.text && priority == 1) {
+      return "Your Passwords Do Not Match!";
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      /*
+       * The code to build the label of the page at the top
+       */
       body: Padding(
         padding: const EdgeInsets.fromLTRB(16, 100, 16, 0),
         child: Column(
@@ -23,56 +48,69 @@ class _SignupPageState extends State<SignupPage> {
             const Text(
               'Sign Up Page',
             ),
+
+            /*
+             * The code to build the username entry of the page, top box
+             */
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextField(
                 controller: username,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'username',
-                ),
+                decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    hintText: 'username',
+                    errorText: errorMessages(0)),
               ),
             ),
+
+            /*
+             * The code to build the password entry of the page, middle box
+             */
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextField(
                 obscureText: true,
                 controller: password1,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'password',
-                ),
+                decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    hintText: 'password',
+                    errorText: errorMessages(1)),
               ),
             ),
+
+            /*
+             * The code to build the password re-entry of the page, bottom box
+             */
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextField(
                 obscureText: true,
                 controller: password2,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'password confirmation',
-                ),
+                decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    hintText: 'password confirmation',
+                    errorText: errorMessages(1)),
               ),
             ),
+
+            /*
+             * The code for the submit button
+             */
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: ElevatedButton(
                 onPressed: () {
-                  if (password1.text != password2.text) {
-                    print("you suck");
-                    /*
-                    * display when you have mismatching passwords
-                    */
-                  } else {
-                    /*
-                    * log the data into server
-                    * send user to home screen
-                    */
-                    print(username.text);
-                    print(password1.text);
-                    print(password2.text);
-                  }
+                  setState(() {
+                    if (errorMessages(0) == null && errorMessages(1) == null) {
+                      /*
+                      * log the data into server
+                      * send user to home screen
+                      */
+                      print(username.text);
+                      print(password1.text);
+                      print(password2.text);
+                    }
+                  });
                 },
                 child: const Text('Sign Up'),
               ),
