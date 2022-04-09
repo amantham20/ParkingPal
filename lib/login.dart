dfaabc0bc025c8f31d88fc7cd28ed014ee7225f1
amantham20 <48414198+amantham20@users.noreply.github.com>
2022-04-09 04:21:54 -0400

    login push 1

--- lib/login.dart
+++ lib/login.dart
@@ -11,8 +11,53 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Login Page'),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 100, 16, 0),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.airline_seat_flat),
+            Text('Login'),
+            // ignore: prefer_const_constructors
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'username',
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'password',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Enter your username',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  print('Login button pressed');
+                },
+                child: const Text('Enabled'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
