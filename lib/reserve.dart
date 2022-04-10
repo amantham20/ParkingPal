c882c68ce1f24270577791a81b330ba09b651711
Rajmeet Singh <chandok1@msu.edu>
2022-04-10 08:29:43 -0400

    reserve page

--- lib/reserve.dart
+++ lib/reserve.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:parkingpal/comp/customdrawabout.dart';
 import 'package:parkingpal/comp/customdrawhome.dart';
+import 'package:parkingpal/comp/customdrawlisting%20copy.dart';
 import 'package:parkingpal/comp/customnav.dart';
+import 'package:parkingpal/globalvar.dart';
+import 'package:parkingpal/home.dart';
+import 'package:parkingpal/login.dart';
 
 class ReservePage extends StatefulWidget {
   const ReservePage({Key? key}) : super(key: key);
@@ -13,30 +17,47 @@ class ReservePage extends StatefulWidget {
 class _ReservePageState extends State<ReservePage> {
   double value = 0;
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    nosecurity = 0;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
-      drawer: const CustomDrawabout(),
-      // Make with a slider for how many hours you want to reserve from 1 to 10.
-      body: SingleChildScrollView(
+      drawer: const CustomDrawlistingRev(),
+      body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.1,
+            const Text(
+              "How many hours do you want to reserve?",
+              style: TextStyle(fontSize: 20),
             ),
-            const Text("Number of hours", style: TextStyle(fontSize: 20)),
             Slider(
               value: value,
               min: 0,
-              max: 10,
-              divisions: 10,
-              onChanged: (newValue) {
+              max: 24,
+              divisions: 24,
+              label: value.toString(),
+              onChanged: (double newValue) {
                 setState(() {
                   value = newValue;
                 });
               },
             ),
-            Text("$value", style: TextStyle(fontSize: 20)),
+            Text(
+              "You have selected " + value.toString() + " hours",
+              style: const TextStyle(fontSize: 20),
+            ),
+            ElevatedButton(
+              child: const Text("Reserve"),
+              onPressed: () {
+                movePage(HomePage(), context);
+              },
+            ),
           ],
         ),
       ),
